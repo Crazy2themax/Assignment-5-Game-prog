f72ecf60dc4d1581463819bbe709ae849bb0e464
Hamington2021 <Serena.shushack@stu.crc-lennox.qc.ca>
2026-04-13 12:47:08 -0400

    Updating
    Updating the score mechanics with game over testing possible and the l10n settings on the game over scene

--- highscore.gd
+++ highscore.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var current_depth = $CanvasLayer/VBoxContainer/VBoxContainer/HBoxContainer/CurrentScoreDepthLabel
+@onready var highscore_depth = $CanvasLayer/VBoxContainer/VBoxContainer/HBoxContainer2/HighScoreDepthLabel
+
 func _ready() -> void:
 	Global.depth_changed.connect(_on_depth_changed)
+	Global.highscore_changed.connect(_on_highscore_changed)
 
 	# If you want the label to show the value immediately on scene load:
 	_on_depth_changed(Global.depth)
+	_on_highscore_changed(Global.highscore)
 
 func _on_depth_changed(value: int) -> void:
-	$MarginContainer/HBoxContainer/Label2.text = str(value)
+	current_depth.text = str(value) + " m"
+
+func _on_highscore_changed(value: int) -> void:
+	highscore_depth.text = str(value) + " m"
